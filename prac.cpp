f1cd2f21abe9e695f7f329ee8fcf5e9b8e4350b1
chezzoba <kaizad@email.com>
2021-05-12 16:11:13 +0200

    Commit to FunctionsCPP branch master by Structures Dpt on May 12 at 16:11

--- prac.cpp
+++ prac.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-#include <vector>
+
+// double pow (double a, double b) {
+//     double x = a;
+//     for (int i = 0; i < 10; i++) {
+//         x = x - ()
+//     }
+// }
 
 using namespace std;
 
 int main(int argc, char **argv) { 
-    double arr[] = {1, 2, 3};
-    cout << len(arr) << endl;
+    
 
 }
